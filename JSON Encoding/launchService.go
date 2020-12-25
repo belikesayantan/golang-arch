2bf8c010369e1d4320d735ad0650818fc729e5ce
Sayantan Paul <belikesayantan12@gmail.com>
2020-12-25 15:44:39 +0530

    Decoding Example

--- JSON Encoding/launchService.go
+++ JSON Encoding/launchService.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+// Animal Type
 type Animal struct {
-	ID int
-	Type string
-	Name string
+	ID    int
+	Type  string
+	Name  string
 	Color string
 }
 
-
 func main() {
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
+	http.HandleFunc("/encode", encode)
+	http.HandleFunc("/decode", decode)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
+	} else {
+		fmt.Println("Server Running at Port: 8080")
 	}
 }
 
-func foo(writer http.ResponseWriter, reader *http.Request) {
+/* curl localhost:8080/encode */
+func encode(writer http.ResponseWriter, reader *http.Request) {
 	cow := Animal{ID: 1, Type: "Mammal", Name: "Bos Taurus", Color: "White"}
 	//dog := Animal{ID: 2, Type: "Mammal", Name: "Canis lupus familiaris", Color: "Yellow"}
+	//cat := Animal{ID: 3, Type: "Mammal", Name: "Felis catus", Color: "Brown"}
 
 	err := json.NewEncoder(writer).Encode(cow)
 	if err != nil {
@@ -36,6 +41,18 @@ func foo(writer http.ResponseWriter, reader *http.Request) {
 	}
 }
 
-func bar(writer http.ResponseWriter, reader *http.Request) {
-	//cat := Animal{ID: 3, Type: "Mammal", Name: "Felis catus", Color: "Brown"}
+/* curl -XGET -H "Content-type: application/json" -d '{"ID":1,"Type":"Mammal","Name":"Bos Taurus","Color":"White"}' 'localhost:8080/decode' */
+func decode(writer http.ResponseWriter, reader *http.Request) {
+	var animal Animal
+	err := json.NewDecoder(reader.Body).Decode(&animal)
+	if err != nil {
+		log.Println("Decoding unsuccessful", err)
+	}
+
+	log.Println("Animal: ", animal)
+
+	/*
+		To use the decode facility use curl command, use the online curl command line builder
+	*/
+
 }
